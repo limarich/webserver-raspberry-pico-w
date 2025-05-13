9825a6ac5624430cc27b8fcac8d40e74d97b6683
lima_rich <richard.esclima@gmail.com>
2025-05-13 15:13:06 -0300

    feat: adiciona debounce na lógica de pressionamento do botão B

--- webserver-raspberry-pico-w.c
+++ webserver-raspberry-pico-w.c
@@ -25,6 +25,7 @@
 #define LED_BLUE_PIN 12               // GPIO12 - LED azul
 #define LED_GREEN_PIN 11              // GPIO11 - LED verde
 #define LED_RED_PIN 13                // GPIO13 - LED vermelho
+#define DEBOUNCE_DELAY 200            // tempo mínimo de debounce do botão
 
 #define DHTPIN 28 // PINO do sensor de umidade e temperatura
 
@@ -37,7 +38,7 @@ volatile float internal_temperature = 0;
 volatile bool green_led_state = false;
 volatile bool blue_led_state = false;
 volatile bool red_led_state = false;
-
+volatile uint32_t last_button_press_time = 0; // tempo de debounce do botão
 // inicializa um led
 void init_led(uint pin);
 // Leitura da temperatura interna
@@ -45,7 +46,13 @@ float internal_temp_read(void);
 
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
-    reset_usb_boot(0, 0);
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    if ((current_time - last_button_press_time) > DEBOUNCE_DELAY)
+    {
+        // Executa a ação do botão (modo BOOTSEL)
+        reset_usb_boot(0, 0);
+        last_button_press_time = current_time;
+    }
 }
 
 int main()
